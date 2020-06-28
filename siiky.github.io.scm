46bda109086205c405c37fa948b2ccad969a4b8b
siiky <siiky@users.noreply.github.com>
2020-06-28 01:54:42 +0100

    Update siiky.github.io.scm to the future SSG

--- siiky.github.io.scm
+++ siiky.github.io.scm
@@ -1,66 +1,57 @@
-(import ssg)
-
-(define css-file "assets/monokai.css")
-(define directories
-  `(
-    "algebra/"
-    "scheme/"
-    "ssg/"
-    "todo/"
-    "words/"
-    )
-  )
+(import (prefix ssg |ssg:|))
 
 (define index
   (idx "nothing interesting here"
        (dir "algebra"
-            (ent "./groups.md"
-                 #:create-date ""
-                 #:title "A quick intro to Group Theory")
-            (ent "./cat_theory_perf.md"
-                 #:create-date "2019/10/15 22:00"
-                 #:title "Category Theory for performance optimization")
-            )
+	    (ent "./groups.md" "" "A quick intro to Group Theory")
+	    (ent "./cat_theory_perf.md" "2019/10/15 22:00" "Category Theory for performance optimization")
+	    )
 
        (dir "scheme"
-            (ent "./exceptions.md"
-                 #:create-date "2019/08/13 20:40"
-                 #:title "Exceptions in Scheme")
-            (ent "./kless.md"
-                 #:create-date "2019/10/13 23:00"
-                 #:title "kless")
-            )
+	    (ent "./exceptions.md" "2019/08/13 20:40" "Exceptions in Scheme")
+	    (ent "./kless.md" "2019/10/13 23:00" "kless")
+	    )
+
+       (dir "server_stuffs"
+	    (ent "./seamless_updates.md" "2020/03/13 23:30" "Seamless Updates")
+	    )
 
        (dir "ssg"
-            (ent "./page.md"
-                 #:create-date "2017/03/06 15:30"
-                 #:title "simple static site generator (idea)")
-            (ent "./lib.md"
-                 #:create-date "2019/10/18 23:00"
-                 #:title "SSG, the Library"
-                 #:wip? #t)
-            (ent "./site.md"
-                 #:create-date ""
-                 #:title "SSG, Your own, personal, Static Site Generator"
-                 #:wip? #t)
-            )
+	    (ent "./page.md" "2017/03/06 15:30" "simple static site generator (idea)")
+	    (ent 'wip "./lib.md" "2019/10/18 23:00" "SSG, the Library")
+	    (ent 'wip "./site.md" "" "SSG, Your own, personal, Static Site Generator")
+	    )
 
        (dir "words"
-            (ent "./slpod.md"
-                 #:create-date "2017/03/04 22:00"
-                 #:title "SLPOD - a simple, suckless podcatcher (idea)")
-            )
+	    (ent "./slpod.md" "2017/03/04 22:00" "SLPOD - a simple, suckless podcatcher (idea)")
+	    (ent "./quotes.md" "2020/05/26 17:00" "just quotes")
+	    )
 
-       (dir "todo"
-            (ent "./sbn.md"
-                 #:create-date ""
-                 #:title "Small Big Numbers"
-                 #:wip? #t)
-            (ent "./gv-dsl.md"
-                 #:create-date ""
-                 #:title "GraphViz Scheme DSL"
-                 #:wip? #t)
-            )
-       ))
+       (dir 'wip "philosophy"
+	    (ent "descartes.discourse_of_a_method_for_the_well_guiding_of_reason_and_the_discovery_of_truth_in_the_sciences.md" "" "_A Discourse of a Method for the Well Guiding of Reason and the Discovery of Truth in the Sciences_, by Descartes")
+	    (ent "descartes.meditations_on_first_philosophy.md" "" "_Meditations on First Philosophy_, by Descartes")
+	    (ent "plato.alcibiades_2.md" "" "_Alcibiades II_, by Plato")
+	    )
 
-(ssg #:index index #:css-file css-file)
+       (dir 'wip "work"
+	    (ent "./credit.md" "2020/04/30" "Credit")
+	    (ent "./to_do_or_not_to_do.md" "2020/04/30" "To do or not to do... That is the Question")
+	    )
+
+       (dir 'wip "functional_programming"
+	    (ent "./immutability.md" "2020/04/10 18:30" "Immutability")
+	    )
+
+       (dir 'wip "todo"
+	    (ent "./sbn.md" "Small Big Numbers")
+	    (ent "./gv-dsl.md" "GraphViz Scheme DSL")
+	    )
+       )
+  )
+
+(ssg:ssg
+  (ssg:site
+    #:css (ssg:css-file "assets/monokai.css")
+    #:index index
+    )
+  )
