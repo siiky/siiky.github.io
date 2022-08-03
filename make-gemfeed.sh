82fdca3d84bf2e5b86f9e24009b777fb1ed81df0
siiky <github-siiky@net-c.cat>
2022-08-03 14:11:57 +0100

    Split gemfeed creation from metadata extraction

--- make-gemfeed.sh
+++ make-gemfeed.sh
@@ -1,21 +1,9 @@
 #!/usr/bin/env sh
-root=$1
+header=$1
+meta=$2
 
-find "$root"/*/ -type f \( -iname '*.md' -or -iname '*.org' -or -iname '*.gmi' \) |
-while read file; do
-  update="$(head -4 "$file" | tail +4 | sed 's|^.*\([0-9]\{4\}/[0-9]\{2\}/[0-9]\{2\}\)\s*$|\1|; s|/|-|g;')"
-  title="$(head -1 "$file" | sed 's|^[^ ]*[ ]*||;')"
+cat "$header"
 
-  # The single quotes let `read` split the words correctly always!
-  echo "$title" | grep -qvw WIP && echo "'$update'\t'$file'\t$title"
-done |
-sort -nr |
-while read date file title; do
-  date="$(echo "$date" | sed "s|^'||; s|'$||;")"
-  file="$(echo "$file" | sed "s|^'||; s|'$||;")"
-
-  # Remove formatting and spaces of the beginning of the line, leaving the title
-  uri="$(echo "$file" | sed "s|^$root/*||; s|^/||;")"
-
-  echo "=> $uri $date $title"
-done
+while IFS='	' read update cdate title uri; do
+  echo "=> $uri $update $title"
+done < "$meta"
---
+++ make-meta.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+root=$1
+
+find "$root"/*/ -type f \( -iname '*.md' -or -iname '*.org' -or -iname '*.gmi' \) |
+while read file; do
+  cdate="$(head -3 "$file" | tail +3 | sed 's|^.*\([0-9]\{4\}/[0-9]\{2\}/[0-9]\{2\}\)\s*$|\1|; s|/|-|g;')"
+  update="$(head -4 "$file" | tail +4 | sed 's|^.*\([0-9]\{4\}/[0-9]\{2\}/[0-9]\{2\}\)\s*$|\1|; s|/|-|g;')"
+  title="$(head -1 "$file" | sed 's|^[^ ]*[ ]*||;')"
+  uri="$(echo "$file" | sed "s|^$root/*||; s|^/||;")"
+
+  # The single quotes let `read` split the words correctly always!
+  echo "$title" | grep -qvw WIP && echo "$update\t$cdate\t$title\t$uri"
+done |
+sort -nr
