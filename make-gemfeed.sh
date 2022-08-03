b869254ae8b17e8b9e55d96ce4bbafafe5de2dae
siiky <github-siiky@net-c.cat>
2022-08-03 13:07:03 +0100

    Make `make-gemfeed.sh` shell compatible

--- make-gemfeed.sh
+++ make-gemfeed.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 root=$1
 
 find "$root"/*/ -type f \( -iname '*.md' -or -iname '*.org' -or -iname '*.gmi' \) |
@@ -7,7 +7,7 @@ while read file; do
   title="$(head -1 "$file" | sed 's|^[^ ]*[ ]*||;')"
 
   # The single quotes let `read` split the words correctly always!
-  echo "$title" | grep -qvw WIP && echo -e "'$update'\t'$file'\t$title"
+  echo "$title" | grep -qvw WIP && echo "'$update'\t'$file'\t$title"
 done |
 sort -nr |
 while read date file title; do
