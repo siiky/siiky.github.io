8b8604db9e1fca67fb96780f664b377346d134bd
siiky <github-siiky@net-c.cat>
2022-08-03 17:42:56 +0100

    Fix Atom feed

--- make-atom.sh
+++ make-atom.sh
@@ -2,15 +2,17 @@
 srht=siiky.srht.site
 
 meta=$1
-last_update="$(head -1 "$meta" | cut -f 1)"
+
+last_update="$(head -1 "$meta" | cut -f 1)T00:00:00Z"
 
 # Write headers
 
 cat <<EOF
 <?xml version="1.0" encoding="utf-8"?>
 <feed xmlns="http://www.w3.org/2005/Atom">
+<link href="https://$srht/atom.xml" rel="self" type="application/atom+xml"/>
 <author><name>siiky</name></author>
-<id>$srht</id>
+<id>https://$srht/</id>
 <title type="text">nothing interesting here</title>
 <updated>$last_update</updated>
 EOF
@@ -20,14 +22,16 @@ EOF
 while IFS='	' read update cdate title uri; do
   html_uri="$(echo "$uri" | sed 's|\.\(gmi\|org\|md\)$|.html|;')"
   html_safe_title="$(echo "$title" | sed 's/&/\&amp;/g; s/</\&lt;/g; s/>/\&gt;/g; s/"/\&quot;/g; s/'"'"'/\&#39;/g')"
+  updated="${update}T00:00:00Z"
+  published="${cdate}T00:00:00Z"
   cat <<EOF
 <entry>
-  <id>$srht/$uri</id>
+  <id>https://$srht/$uri</id>
   <title type="text">$html_safe_title</title>
-  <updated>$update</updated>
-  <published>$cdate</published>
-  <link href="https://$srht/$html_uri" rel="alternate" type="text/html" />
-  <link href="gemini://$srht/$uri" rel="alternate" type="text/gemtext" />
+  <updated>$updated</updated>
+  <published>$published</published>
+  <link href="https://$srht/$html_uri" rel="alternate" type="text/html"/>
+  <link href="gemini://$srht/$uri" rel="alternate" type="text/gemtext"/>
 </entry>
 EOF
 done < "$meta"
