df200639bae5428f0b0940663f4d861721ac072e
siiky <github-siiky@net-c.cat>
2023-01-06 10:32:49 +0000

    Print HTML headers to determine encoding

--- md2html.scm
+++ md2html.scm
@@ -4,8 +4,26 @@
   lowdown
   )
 
+(define-constant before
+#<<EOF
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="" xml:lang="">
+<head><meta charset="utf-8"/></head>
+<body>
+EOF
+)
+
+(define-constant after
+#<<EOF
+
+</body>
+</html>
+EOF
+)
+
 (with-output-to-file
   (car (command-line-arguments))
   (lambda ()
+    (print before)
     (markdown->html)
-    ))
+    (print after)))
