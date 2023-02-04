8a2d1233183594e26fb2ef452b2ac19e1250964c
siiky <github-siiky@net-c.cat>
2023-02-04 00:46:56 +0000

    Update reclog

--- make-site-meta.sh
+++ make-site-meta.sh
@@ -5,10 +5,10 @@ while read file; do
   title="$(head -1 "$file" | sed 's|^[^ ]*[ ]*||;')"
   #author="$(head -2 "$file" | tail +2 | sed 's|^@*||;')"
   cdate="$(head -3 "$file" | tail +3 | sed 's|^.*\([0-9]\{4\}/[0-9]\{2\}/[0-9]\{2\}\)\s*$|\1|; s|/|-|g;')"
-  significant_update="$(head -4 "$file" | tail +4 | sed 's|^.*\([0-9]\{4\}/[0-9]\{2\}/[0-9]\{2\}\)\s*$|\1|; s|/|-|g;')"
+  update="$(head -4 "$file" | tail +4 | sed 's|^.*\([0-9]\{4\}/[0-9]\{2\}/[0-9]\{2\}\)\s*$|\1|; s|/|-|g;')"
   uri="$(echo "$file" | sed "s|^$root/*||; s|^/||;")"
 
   # The single quotes let `read` split the words correctly always!
-  echo "$title" | grep -qvw WIP && echo "${significant_update}\t${cdate}\t${title}\t${uri}"
+  echo "$title" | grep -qvw WIP && echo "${update}\t${cdate}\t${title}\t${uri}"
 done |
 sort -nr
--- make-wiki-meta.sh
+++ make-wiki-meta.sh
@@ -6,11 +6,11 @@ while read file; do
   title="$(head -1 "$file" | sed 's|^[^ ]*[ ]*||;')"
   #author="$(head -2 "$file" | tail +2 | sed 's|^@*||;')"
   cdate="$(head -3 "$file" | tail +3 | sed 's|^.*\([0-9]\{4\}/[0-9]\{2\}/[0-9]\{2\}\)\s*$|\1|; s|/|-|g;')"
-  signifcant_update="$(head -4 "$file" | tail +4 | sed 's|^.*\([0-9]\{4\}/[0-9]\{2\}/[0-9]\{2\}\)\s*$|\1|; s|/|-|g;')"
-  update="$(head -5 "$file" | tail +5 | sed 's|^.*\([0-9]\{4\}/[0-9]\{2\}/[0-9]\{2\}\)\s*$|\1|; s|/|-|g;')"
+  update="$(head -4 "$file" | tail +4 | sed 's|^.*\([0-9]\{4\}/[0-9]\{2\}/[0-9]\{2\}\)\s*$|\1|; s|/|-|g;')"
+  minor_update="$(head -5 "$file" | tail +5 | sed 's|^.*\([0-9]\{4\}/[0-9]\{2\}/[0-9]\{2\}\)\s*$|\1|; s|/|-|g;')"
   tags="$(head -6 "$file" | tail +6)"
 
   # The single quotes let `read` split the words correctly always!
-  echo "$title" | grep -qvw WIP && echo "${signifcant_update}\t${update}\t${cdate}\t${title}\t${uri}\t${tags}"
+  echo "$title" | grep -qvw WIP && echo "${update}\t${minor_update}\t${cdate}\t${title}\t${uri}\t${tags}"
 done |
 sort -nr
